7b23749ae4765e610450e88cf0f5023bafead863
Gerben <gerbenvaneerten@gmail.com>
2017-08-10 12:56:14 +0200

    Added common bit masks utility header

---
+++ src/BitMasks.h
@@ -0,0 +1,89 @@
+/*
+Common bit patterns
+*/
+#ifndef _BIT_MASKS_H
+#define _BIT_MASKS_H
+
+#define BIT_0   0x01
+#define BIT_1   0x02
+#define BIT_2   0x04
+#define BIT_3   0x08
+#define BIT_4   0x10
+#define BIT_5   0x20
+#define BIT_6   0x40
+#define BIT_7   0x80
+
+
+#define BIT_0_1 0x03
+#define BIT_0_2 0x07
+#define BIT_0_3 0x0F
+#define BIT_0_4 0x1F
+#define BIT_0_5 0x3F
+#define BIT_0_6 0x7F
+#define BIT_0_7 0xFF
+
+#define BIT_1_2 0x06
+#define BIT_1_3 0x0E
+#define BIT_1_4 0x1E
+#define BIT_1_5 0x3E
+#define BIT_1_6 0x7E
+#define BIT_1_7 0xFE
+
+#define BIT_2_3 0x0C
+#define BIT_2_4 0x1C
+#define BIT_2_5 0x3C
+#define BIT_2_6 0x7C
+#define BIT_2_7 0xFC
+
+#define BIT_3_4 0x18
+#define BIT_3_5 0x38
+#define BIT_3_6 0x78
+#define BIT_3_7 0xF8
+
+#define BIT_4_5 0x30
+#define BIT_4_6 0x70
+#define BIT_4_7 0xF0
+
+#define BIT_5_6 0x60
+#define BIT_5_7 0xE0
+
+#define BIT_6_7 0xC0
+
+
+#define BIT_1_0 0x03
+#define BIT_2_0 0x07
+#define BIT_3_0 0x0F
+#define BIT_4_0 0x1F
+#define BIT_5_0 0x3F
+#define BIT_6_0 0x7F
+#define BIT_7_0 0xFF
+
+#define BIT_2_1 0x06
+#define BIT_3_1 0x0E
+#define BIT_4_1 0x1E
+#define BIT_5_1 0x3E
+#define BIT_6_1 0x7E
+#define BIT_7_1 0xFE
+
+#define BIT_3_2 0x0C
+#define BIT_4_2 0x1C
+#define BIT_5_2 0x3C
+#define BIT_6_2 0x7C
+#define BIT_7_2 0xFC
+
+#define BIT_4_3 0x18
+#define BIT_5_3 0x38
+#define BIT_6_3 0x78
+#define BIT_7_3 0xF8
+
+#define BIT_5_4 0x30
+#define BIT_6_4 0x70
+#define BIT_7_4 0xF0
+
+#define BIT_6_5 0x60
+#define BIT_7_5 0xE0
+
+#define BIT_7_6 0xC0
+
+#endif
+
